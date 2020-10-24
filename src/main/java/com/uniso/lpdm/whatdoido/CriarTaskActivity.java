118e16e878162a01156fe7b0311ac2e46c956666
Bianca da Silva Campos <51832213+BiancaSCampos@users.noreply.github.com>
2020-10-24 18:44:31 -0300

    Add files via upload

---
+++ src/main/java/com/uniso/lpdm/whatdoido/CriarTaskActivity.java
@@ -0,0 +1,15 @@
+package com.uniso.lpdm.whatdoido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriarTaskActivity extends AppCompatActivity {
+
+    //Tela de Criação de uma Task
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_task);
+    }
+}
---
+++ src/main/java/com/uniso/lpdm/whatdoido/TaskActivity.java
@@ -0,0 +1,16 @@
+package com.uniso.lpdm.whatdoido;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+
+    //Essa deve ser a tela que vai ser mostrada ao clicar em determinada Task.
+}
